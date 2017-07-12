f3c4bbdeb206d6dd86c6a0d4d1fdeaeb261ea8d2
kerkins <brumeregan@gmail.com>
2017-07-12 14:06:33 +0300

    Added editing of article

--- blogapp/views.py
+++ blogapp/views.py
@@ -5,7 +5,7 @@ from .forms import PostForm
 
 
 def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('-published_date')
     return render(request, 'blogapp/post_list.html', {'posts': posts})
 
 
@@ -25,4 +25,19 @@ def post_new(request):
             return redirect('post_details', pk=post.pk)
     else:
         form = PostForm()
+    return render(request, 'blogapp/post_edit.html', {'form': form})
+
+
+def post_edit(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    if request.method == 'POST':
+        form = PostForm(request.POST, instance=post)
+        if form.is_valid():
+            post.author = request.user
+            post.published_date = timezone.now()
+            post = form.save()
+            post.save()
+            return redirect('post_details', pk=post.pk)
+    else:
+        form = PostForm(instance=post)
     return render(request, 'blogapp/post_edit.html', {'form': form})
